2d397655320aa40e53bc496ad9335cab259de854
aaron462 <78084178+aaron462@users.noreply.github.com>
2024-10-29 13:54:59 +0000

    skibiid

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const nggUrl = 'https://mathsspot.com';
+
+const proxy = createProxyMiddleware({
+  target: nggUrl,
+  changeOrigin: true,
+  secure: true,
+  logLevel: 'debug',
+  router: function (req) {
+    if (req.headers.host === 'https://selfstudybrain.com/') {
+      req.headers['X-Forwarded-For'] = '';
+      req.headers['X-Real-IP'] = '';
+      req.headers['Via'] = '';
+    }
+    return nggUrl;
+  },
+});
+
+app.use('/', proxy);
+
+const port = process.env.PORT || 24433;
+app.listen(port, () => {
+  console.log(`roblox proxy is online http://localhost:24433`);
+});
